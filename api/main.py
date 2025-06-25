dcc05fbbc8a3ad225abfb1226f39684a16204c69
crocodilelurker <swagatsahu556@gmail.com>
2025-06-25 23:18:14 +0530

    Did Router Parts for Fernet API

--- api/main.py
+++ api/main.py
@@ -1,42 +1,4 @@
-from fastapi import FastAPI,APIRouter,HTTPException
-from cryptography.fernet import Fernet
-from pydantic import BaseModel
+from fastapi import FastAPI
+from routes.routes import router
 app=FastAPI()
-router=APIRouter()
-key=""
-class EncrpytRequest(BaseModel):
-    key: str
-    data: str
-@router.get("/generate_key")
-async def generate_key():
-    try:
-        key= Fernet.generate_key()
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    return{
-        "key":key
-    }
-@router.get("/")
-def root():
-    return {"message": "Welcome to the Flowgrid fernet api"}
-@router.post("/encrypt")
-async def encrypt(request: EncrpytRequest):
-    try:
-        fernet = Fernet(request.key.encode())
-        encrypted_data = fernet.encrypt(request.data.encode())
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    return {
-        "encrypted_data": encrypted_data
-    }
-@router.post("/decrypt")
-async def decrypt(request: EncrpytRequest):
-    try:
-        fernet = Fernet(request.key.encode())
-        decrypted_data = fernet.decrypt(request.data.encode()).decode()
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    return {
-        "decrypted_data": decrypted_data
-    }
 app.include_router(router)
