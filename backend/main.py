c6a5b5839c03dba93c1dfd82f70891dde0fc4ca8
crocodilelurker <swagatsahu556@gmail.com>
2025-06-25 23:24:22 +0530

    Removed Unuseful commits

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,7 @@
-from fastapi import APIRouter
 from fastapi import FastAPI
 from configurations import connectDB
 from contextlib import asynccontextmanager
 from routes.route import router
-from database.models import User, Transaction
-from configurations import UserCollection, TransactionCollection
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     connectDB()
